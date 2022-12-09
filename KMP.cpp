4041006111ee93cf0d1786b900919dadbd226934
Bober-G <80640293+Bober-G@users.noreply.github.com>
2022-12-09 17:04:45 +0100

    Update KMP.cpp
    refactor KMP

--- KMP.cpp
+++ KMP.cpp
@@ -9,7 +9,7 @@ void LPS(string pi, vector<int>& lps) {
     lps[0] = 0;
     int len = 0;
     int i = 1;
-    
+
     while (i < pi.length()) {
         if (pi[i] == pi[len]) {
             len++;
@@ -33,35 +33,34 @@ void LPS(string pi, vector<int>& lps) {
 
 int KMP(string pattern, string pi) {
     bool isInFile = false;
-    unsigned long long n = pi.length();
-    unsigned long long m = pattern.length();
-    vector<int>lps(m);
+    unsigned long long piExpansionLength = pi.length();
+    unsigned long long patternLength = pattern.length();
+    vector<int>lps(patternLength);
 
-    LPS(pattern, lps); // This function constructs the Lps array.
+    LPS(pattern, lps);
 
     int i = 0;
     int j = 0;
     int index;
-    
-    while (i < n) {
-        if (pattern[j] == pi[i]) { 
-            i++; 
-            j++; 
-        } // If there is a match continue.
-        if (j == m) {
+
+    while (i < piExpansionLength) {
+        if (pattern[j] == pi[i]) {
+            i++;
+            j++;
+        } 
+        if (j == patternLength) {
             cout << pattern << ',';
-            cout << i - m << endl;    // if j==m it is confirmed that we have found the pattern and we output the index
-            // and update j as Lps of last matched character.
+            cout << i - patternLength << endl; // if j==m the pattern is found and we output the index and update j as Lps of last matched character.
             j = lps[j - 1];
-            index = i - m;
-            break;
+            index = i - patternLength;
+            break; // after finding a match break
         }
-        else if (i < n && pattern[j] != pi[i]) {  // If there is a mismatch
+        else if (i < piExpansionLength && pattern[j] != pi[i]) {  // pattern does not much
             if (j == 0) {
                 i++;
-            }         // if j becomes 0 then simply increment the index i
+            }
             else {
-                j = lps[j - 1];  // Update j as Lps of last matched character
+                j = lps[j - 1];
             }
         }
     }
@@ -70,19 +69,19 @@ int KMP(string pattern, string pi) {
 
 string loadFile(const string& path) {
     ifstream file;
-    
+
     file.open(path);
-    
+
     if (file.fail()) {
         cout << "File open failed" << endl;
     }
 
     stringstream buffer;
-    
+
     buffer << file.rdbuf();
-    
+
     string file_contents;
-    
+
     file_contents = buffer.str();
 
     file.close();
@@ -95,12 +94,12 @@ struct Arguments {
     string pattern;
 };
 
-Arguments parseArguments(int argc, char *argv[]) {
+Arguments parseArguments(int argc, char* argv[]) {
     Arguments args;
-    for (int i = 0; i < argc; i++) {   
+    for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
             args.inputFilePath = argv[i + 1];
-        } 
+        }
         else if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
             args.pattern = argv[i + 1];
         }
@@ -108,20 +107,20 @@ Arguments parseArguments(int argc, char *argv[]) {
     return args;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     try {
         Arguments args = parseArguments(argc, argv);
-        
-        string pi = loadFile("C:/Private/Algs/PiGenerator/PiGenerator/pi.txt");
+
+        string pi = loadFile("pi.txt");
         // string pi = loadFile(args.inputFilePath);
 
-        string pattern = "1569817";
-        // string pattern = args.pattern;
+        //string pattern = "1569817";
+         string pattern = args.pattern;
 
         int index = KMP(pattern, pi);
 
         ofstream out("output.txt");
-        out << pattern << ',' << index;
+        out << pattern << ',' << index; // output to file the form pattern,index
         out.close();
     }
     catch (...) {
