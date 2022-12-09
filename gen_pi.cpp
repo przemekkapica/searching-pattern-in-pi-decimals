171fa6b545c40b7b87e9cfdea4c4b8e24e5e90e9
Jakub Woch <jawo@netcompany.com>
2022-12-09 15:49:42 +0100

    generating pi refactor

--- gen_pi.cpp
+++ gen_pi.cpp
@@ -9,16 +9,20 @@ struct BinarySplit {
     mpz_class D, H, L;
 };
 
+struct Arguments {
+    int piDigits;
+};
+
 class Chudnovsky {
     mpz_class A, B, C, D, E, C3_24; // Multi precision integers
-    int DIGITS, PRECISION, N;            
-    double DIGITS_PER_TERM;        
+    int DIGITS, PRECISION, N;
+    double DIGITS_PER_TERM;
     clock_t start, end; // Timestamps
     BinarySplit computeWithBinarySplit(int n1, int n2); // Computes using Binary Split Algorithm
 
-    public:
-        Chudnovsky(int digits);                   
-        void getPiExpansion();            
+public:
+    Chudnovsky(int digits);
+    void getPiExpansion();
 };
 
 Chudnovsky::Chudnovsky(int digits) {
@@ -35,26 +39,51 @@ Chudnovsky::Chudnovsky(int digits) {
     PRECISION = DIGITS * log2(10);
 }
 
+int multiply_bitewise(int a, int b)
+{
+    int n1 = abs(a), n2 = abs(b), result = 0;
+    bool neg = false;
+    if (min(a, b) < 0 && max(a, b) >= 0) neg = true;
+    while (n2 > 0)
+    {
+        if (n2 & 1 == 1) result += n1;
+        n2 >>= 1; n1 <<= 1;
+    }
+    if (neg) return (~(result)+1);
+    else return result;
+}
 
+int Add(int x, int y)
+{
+    while (y != 0)
+    {
+        unsigned carry = x & y;
+        x = x ^ y;
+        y = carry << 1;
+    }
+    return x;
+}
 // Compute BinarySplit (by Binary Splitting Algorithm)
 // ref: https://en.wikipedia.org/wiki/Binary_splitting#:~:text=In%20mathematics%2C%20binary%20splitting%20is,hypergeometric%20series%20at%20rational%20points.
-BinarySplit Chudnovsky::computeBinarySplit(int n1, int n2) {
+BinarySplit Chudnovsky::computeWithBinarySplit(int n1, int n2) {
     int m;
     BinarySplit result;
 
     if (n1 + 1 == n2) {
-        result.D  = (2 * n2 - 1);
-        result.D *= (6 * n2 - 1);
-        result.D *= (6 * n2 - 5);
-        result.H  = C3_24 * n2 * n2 * n2;
-        result.L  = (A + B * n2) * result.D;
+        int t = multiply_bitewise(6, n2);
+        result.D = ((n2 >> 1) - 1);
+        result.D *= (t - 1);
+        result.D *= (t - 5);
+        result.H = C3_24 * n2 * n2 * n2;
+        result.L = (A + B * n2) * result.D;
         if ((n2 & 1) == 1) {
-            result.L = - result.L;
+            result.L = -result.L;
         }
-    } else {
+    }
+    else {
         m = (n1 + n2) / 2;
-        BinarySplit reccBinarySplit1 = computeBinarySplit(n1, m);
-        BinarySplit reccBinarySplit2 = computeBinarySplit(m, n2);
+        BinarySplit reccBinarySplit1 = computeWithBinarySplit(n1, m);
+        BinarySplit reccBinarySplit2 = computeWithBinarySplit(m, n2);
         result.D = reccBinarySplit1.D * reccBinarySplit2.D;
         result.H = reccBinarySplit1.H * reccBinarySplit2.H;
         result.L = reccBinarySplit1.L * reccBinarySplit2.H + reccBinarySplit1.D * reccBinarySplit2.L;
@@ -70,7 +99,7 @@ void Chudnovsky::getPiExpansion() {
     start = clock();
 
     // PI computation starts here
-    BinarySplit split = computeBinarySplit(0, N);
+    BinarySplit split = computeWithBinarySplit(0, N);
     mpf_class pi(0, PRECISION);
     pi = D * sqrt((mpf_class)E) * split.H;
     pi /= (A * split.H + split.L);
@@ -80,31 +109,26 @@ void Chudnovsky::getPiExpansion() {
     cout << "Computation took " << (double)(end - start) / CLOCKS_PER_SEC << " seconds." << endl;
 
     // Outputs to file
-    ofstream ofs ("pi.txt");
+    ofstream ofs("pi.txt");
     ofs.precision(DIGITS + 1);
     ofs << pi << endl;
 }
 
-struct Arguments {
-    int piDigits;
-};
-
-Arguments parseArguments(int argc, char *argv[]) {
+Arguments parseArguments(int argc, char* argv[]) {
     Arguments args;
-    for (int i = 0; i < argc; i++) {   
+    for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-d") == 0 && i + 1 < argc) {
             args.piDigits = atoi(argv[i + 1]);
-        } 
+        }
     }
     return args;
 }
 
-int main(int argc, char *argv[]) {
-    try { 
+int main(int argc, char* argv[]) {
+    try {
         Arguments args = parseArguments(argc, argv);
-        
-        Chudnovsky chudnovsky;
-        chudnovsky(args.piDigits);
+
+        Chudnovsky chudnovsky = Chudnovsky(args.piDigits);
 
         // Computes pi and saves the result to text file
         chudnovsky.getPiExpansion();
